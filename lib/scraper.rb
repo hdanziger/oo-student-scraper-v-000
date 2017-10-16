48c3c1d95bb3dce6d4129864425ac6ec6ba593dd
Hannah Danziger <hdanziger8@gmail.com>
2017-10-16 13:43:43 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,45 @@
+
 require 'open-uri'
 require 'pry'
 
+
 class Scraper
 
-  def self.scrape_index_page(index_url)
-    
+  def self.scrape_index_page(index_url = "http://students.learn.co/")
+    #binding.pry
+    index_page = Nokogiri::HTML(open("http://students.learn.co/"))
+    students = []
+    index_page.css("div.roster-cards-container").each do |card|
+      card.css(".student-card a").each do |student|
+        student_name = student.css('.student-name').text
+        student_location = student.css('.student-location').text
+        student_profile_url = "fixtures/student-site/#{student.attr('href')}"
+        students << {name: student_name, location: student_location, profile_url: student_profile_url}
+    end
   end
-
-  def self.scrape_profile_page(profile_url)
-    
-  end
-
+  students
 end
 
+
+
+def self.scrape_profile_page(profile_url = "http://students.learn.co/students/laura-correa.html")
+  student = {}
+  profile_page = Nokogiri::HTML(open("http://students.learn.co/students/laura-correa.html"))
+  links = profile_page.css(".social-icon-container").children.css("a").map { |el| el.attribute('href').value}
+  links.each do |link|
+    if link.include?("linkedin")
+      student[:linkedin] = link
+      student[:github] = link
+    elsif link.include?("twitter")
+      student[:twitter] = link
+    else
+      student[:blog] = link
+    end
+  end
+
+  student[:profile_quote] = profile_page.css(".profile-quote").text if profile_page.css(".profile-quote")
+  student[:bio] = profile_page.css("div.bio-content.content-holder div.description-holder p").text if profile_page.css("div.bio-content.content-holder div.description-holder p")
+
+  student
+  end
+end
